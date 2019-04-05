2be7f34d3d1c101180682a280afdf0aa9e05d9f3
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-05 09:01:33 -0700

    javascript todo list

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//TODO: What to do
+
+//TODO: What to track
+//TODO: Track Number Of Wins
+//TODO: Track Number Of Losses
+//TODO: Choose A Target Number
+//TODO: Your Total Score
+//TODO: Item 1 needs a radom number 
+//TODO: Item 2 needs a radom number 
+//TODO: Item 3 needs a radom number 
+//TODO: Item 4 needs a radom number 
+
+//TODO: Use key events for numbers that players press
+
+//TODO: Check if users won
+//TODO: Check if user lost
+
+//TODO: Diplay 4 objects
+//TODO: Display the wins
+//TODO: Display the looses
+
+
+//TODO: Prompt message after user looses game
+
+//TODO: Restart the Game
