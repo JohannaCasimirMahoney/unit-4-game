6fcada6aecdd9c040a7d18757929effacf2e3833
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-05 14:38:31 -0700

    added chess piece picture

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-//TODO: What to do
 
 //TODO: What to track
 //TODO: Track Number Of Wins
@@ -6,6 +5,9 @@
 //TODO: Choose A Target Number
 //TODO: Your Total Score Is
 
+
+
+//TODO: What to do
 //TODO: Each Item needs a radom hidden number between 1-20
 
 //TODO: Add Item 1 needs a radom number 
@@ -13,7 +15,12 @@
 //TODO: Add Item 3 needs a radom number 
 //TODO: Add Item 4 needs a radom number 
 
-//TODO: Use key events for numbers that players press
+//TODO: Click Item 1 needs a radom number 
+//TODO: Click Item 2 needs a radom number 
+//TODO: Click Item 3 needs a radom number 
+//TODO: Click Item 4 needs a radom number 
+
+
 //TODO: Add clicks to each item to add points to user score
 
 //TODO: Check if users won
@@ -27,3 +34,8 @@
 //TODO: Prompt message after user looses game
 
 //TODO: Restart the Game
+
+var wins = 0;
+var looses = 0;
+
+//TODO: Use key events for numbers that players press
