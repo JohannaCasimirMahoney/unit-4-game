f442acc45d639a29ca22d20a7f3a53b199dca9e7
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-06 09:06:55 -0700

    added last two chess pieces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,11 @@
 //TODO: Track Number Of Losses
 //TODO: Trak A Target Number
 //TODO: Score
-//TODO: Track Chess Piece 
+//TODO: Track Each Chess Piece 
 
 var wins = 0;
-var looses = 0;
-var TargetNumber = 0;
+var losses = 0;
+var targetNumber = 0;
 var score = 0;
 var king = 0;
 var queen = 0;
@@ -14,7 +14,7 @@ var bishop = 0;
 var knight = 0;
 
 function settarget() {
-    TargetNumber = Math.floor(Math.random() * 29) + 1;
+    targetNumber = Math.floor(Math.random() * 40) + 1;
 }
 
 
@@ -22,31 +22,49 @@ function startgame() {
     settarget();
     score = 0;
 
-    king = Math.floor(Math.random() * 10) + 1; //add for all the other pieces
-    queen = Math.floor(Math.random() * 15) + 1; //add for all the other pieces
+    //Add random numbers for each chess pieces
+    king = Math.floor(Math.random() * 20) + 1;
+    queen = Math.floor(Math.random() * 15) + 1;
+    bishop = Math.floor(Math.random() * 10) + 1;
+    knight = Math.floor(Math.random() * 5) + 1;
 
+    //Display of wins,losses,score, and target number
 
     $("#score").text("Score: " + score.toString());
     $("#wins").text("Wins: " + wins.toString());
-    $("#targetNumber").text("Target Number: " + TargetNumber.toString());
+    $("#targetNumber").text("Target Number: " + targetNumber.toString());
+    $("#losses").text("Losses: " + losses.toString());
 }
-
-
 
 $(document).ready(function () {
     startgame();
+
+    //TODO: Add clicks to each item to add points to user score
+
     // $("#wins").css('color', 'white');
     $("#king").click(function () {
         // $("#wins").css('color', 'white');
-        $("#debug").text("you choose the king");
+        $("#debug").text("You Choose The King");
         score += king;
         $("#score").text("Score: " + score.toString());
     });
 
     $("#queen").click(function () {
         // $("#wins").css('color', 'white');
-        $("#debug").text("you choose the queen");
+        $("#debug").text("You Choose The Queen");
         score += queen;
+        $("#score").text("Score: " + score.toString());
+    });
+    $("#bishop").click(function () {
+        // $("#wins").css('color', 'white');
+        $("#debug").text("You Choose The Bishop");
+        score += bishop;
+        $("#score").text("Score: " + score.toString());
+    });
+    $("#knight").click(function () {
+        // $("#wins").css('color', 'white');
+        $("#debug").text("You Choose The Knight");
+        score += knight;
         $("#score").text("Score: " + score.toString());
     });
 
@@ -55,20 +73,12 @@ $(document).ready(function () {
 
 
 
-//TODO: Add Item 1 needs a radom number 
-//TODO: Add Item 2 needs a radom number 
-//TODO: Add Item 3 needs a radom number 
-//TODO: Add Item 4 needs a radom number 
-
-
-//TODO: Add clicks to each item to add points to user score
 
 //TODO: Check if users won
 //TODO: Check if user lost
 
-//TODO: Diplay 4 objects
-//TODO: Display the wins
-//TODO: Display the looses
+
+
 
 
 //TODO: Prompt message after user looses game
@@ -76,7 +86,3 @@ $(document).ready(function () {
 //TODO: Restart the Game
 
 
-
-
-
-//TODO: Use key events for numbers that players press
