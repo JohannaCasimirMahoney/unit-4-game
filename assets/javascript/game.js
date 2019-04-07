1bcf8ef7fe1bdebe81c245e9eedddf0c069d781a
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-06 21:00:59 -0700

    last edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-//TODO: Track Number Of Wins
-//TODO: Track Number Of Losses
-//TODO: Trak A Target Number
-//TODO: Score
-//TODO: Track Each Chess Piece 
+// Track Number Of Wins
+// Track Number Of Losses
+// Trak A Target Number
+// Track Score
+// Track Each Chess Piece 
 
 var wins = 0;
 var losses = 0;
@@ -13,8 +13,6 @@ var queen = 0;
 var bishop = 0;
 var knight = 0;
 
-
-
 function settarget() {
     targetNumber = Math.floor(Math.random() * 30) + 1;
 }
@@ -24,7 +22,7 @@ function startgame() {
     settarget();
     score = 0;
 
-    //Add random numbers for each chess pieces
+    //Random numbers for each chess pieces
     king = Math.floor(Math.random() * 20) + 1;
     queen = Math.floor(Math.random() * 10) + 1;
     bishop = Math.floor(Math.random() * 10) + 1;
@@ -40,12 +38,13 @@ function startgame() {
     $("#targetNumber").text("Target Number: " + targetNumber.toString());
 
 }
+
 //wins
 function youWin() {
     wins++;
     $("#wins").html("Wins: " + wins);
     alert("Awsome! ");
-    newGame();
+
 }
 
 //losses
@@ -53,9 +52,9 @@ function youLose() {
     losses++;
     $("#losses").html("Losses: " + losses);
     alert("Better Luck Next Time");
-    newGame();
-};
 
+
+};
 
 $(document).ready(function () {
     startgame();
@@ -122,13 +121,10 @@ $(document).ready(function () {
         if (score > targetNumber) {
             youLose();
         }
+
     });
 
 });
 
-// Start & Reset Game
 
-//TODO: Check if users won
-//TODO: Check if user lost
 
-//TODO: Restart the Game
